daecb8e9d9cccf95248d9fbe5d98d07986777ff1
maxime cauchy <maxime.cauchy93@gmail.com>
2025-09-30 16:20:56 +0200

    Initial Commit

---
+++ simpleDecision/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  simpleDecision
+//
+//  Created by Tristan Aranda  on 30/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ simpleDecision/simpleDecisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleDecisionApp.swift
+//  simpleDecision
+//
+//  Created by Tristan Aranda  on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct simpleDecisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
